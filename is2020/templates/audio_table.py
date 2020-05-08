4f91d29e5d5cd21c243809a7218446d91a51c393
zjliu <sorcererliu@gmail.com>
2020-05-09 00:17:02 +0800

    add demo for is2020

---
+++ is2020/templates/audio_table.py
@@ -0,0 +1,68 @@
+from math import ceil
+from is2020.templates.frame import header, footer
+
+
+def audio_line(audios, titles):
+    """
+    Two line tr, one for title and another for audio
+    """
+    title_line = ["<tr>", "</tr>"]
+    for title in titles:
+        if title is not None:
+            title_line.insert(-1, f"<td><strong>{title}</strong></td>")
+        else:
+            title_line.insert(-1, "<td></td>")
+    audio_line = ["<tr>", "</tr>"]
+    for audio in audios:
+        if audio is not None:
+            audio_line.insert(-1, f"""<td><audio controls=""><source src="{audio}"></audio></td>""")
+        else:
+            audio_line.insert(-1, "<td></td>")
+    return "".join(title_line) + "".join(audio_line)
+
+
+def audio_lines(audios, titles, width=2):
+    """
+    Many lines of width ended with empty line.
+    """
+    l = []
+    for i in range(0, ceil(len(audios) / width)):
+        audio_buf = []
+        title_buf = []
+        begin = i * width
+        end = begin + width
+        while begin < end:
+            if begin < len(audios):
+                audio_buf.append(audios[begin])
+                title_buf.append(titles[begin])
+            else:
+                audio_buf.append(None)
+                title_buf.append(None)
+            begin += 1
+        l.append(audio_line(audio_buf, title_buf))
+    return "".join(l) + "<tr>" + "<td></td>" * width + "</tr>"
+
+
+def table_of_list(text, paths, titles, width=2):
+    return f"""
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th colspan={width} style="text-align: left;">{text}</th>
+                </tr>
+            </thead>
+            <tbody>
+            {audio_lines(paths, titles, width)}
+            </tbody>
+        </table>
+    </div>
+    """
+
+
+def table_with_texts(texts, pathss, titless, width=2):
+    m = [
+        table_of_list(text, paths, titles, width) for text, paths, titles in zip(texts, pathss, titless)
+    ]
+    return "".join(m)
+
---
+++ is2020/templates/frame.py
@@ -0,0 +1,30 @@
+
+def header(title):
+    header = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>{title}</title>
+
+        <!-- Bootstrap -->
+        <link href="statics/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+
+    <div class="container">
+    """
+    return header
+
+def footer():
+    footer = """
+    </div>
+    <script src="statics/jquery/jquery-1.12.4.min.js"></script>
+    <script src="statics/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+    </html>
+    """
+    return footer
